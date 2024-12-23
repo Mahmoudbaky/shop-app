7c08055fa05fdd5c1d2f7a4bd15bba885142cd20
MAHMOUD <ma7mouudbaky@gmail.com>
2024-12-23 11:51:45 +0300

    finishing loging styling

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { supabase } from "@/app/utils/supabaseClient";
 import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
+
 import Link from "next/link";
 
 const login = () => {
@@ -34,42 +35,51 @@ const login = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen ">
-      {/* <div className="bg-neutral xl:w-[600px] md:w-[500px] h-full"></div> */}
-      <form className="space-y-4 w-[350px] ">
-        <div className=" flex flex-col gap-2">
-          <h1 className="text-2xl font-bold"> Sgin in </h1>
-          <p className="">Enter your email and password to sign in!</p>
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center h-screen">
+      {/* /* <!-- Image Column --> */}
+      <div className="hidden bg-accent md:block md:w-full md:h-full md:my-8 xl:my-12">
+        <div className="flex flex-col items-center justify-center xl:min-w-full md:min-w-full min-h-full gap-8 md:w-[600px] xl:w-[600px]  mx-auto">
+          {/* <p className="text-2xl text-white self-start">Welcome to our shop</p> */}
+          <img
+            src="/images/rb_9090.png"
+            alt="image"
+            className="w-full h-auto xl:w-[600px] md:w-[600px]"
+          />
         </div>
+      </div>
+      <div className="flex items-center justify-center p-8">
+        <form className="space-y-4 w-[350px] mx-auto ">
+          <div className=" flex flex-col gap-2">
+            <h1 className="text-2xl font-bold"> Sgin in </h1>
+            <p className="">Enter your email and password to sign in!</p>
+          </div>
 
-        <button className="w-full btn  border-gray-400">
-          <FcGoogle className="text-2xl" />
-          <span> Google</span>
-        </button>
-        <hr className="text-red-900" />
+          <button className="w-full btn  border-gray-400">
+            <FcGoogle className="text-2xl" />
+            <span> Google</span>
+          </button>
+          <hr className="text-red-900" />
 
-        <label className="input input-bordered flex items-center gap-2 border-gray-400">
-          Name
-          <input type="text" className="grow " placeholder="Daisy" />
-        </label>
-        <label className="input input-bordered flex items-center gap-2 border-gray-400">
-          Email
-          <input type="text" className="grow" placeholder="daisy@site.com" />
-        </label>
+          <label className="input input-bordered flex items-center gap-2 border-gray-400">
+            Name
+            <input type="text" className="grow " placeholder="Daisy" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2 border-gray-400">
+            Email
+            <input type="text" className="grow" placeholder="daisy@site.com" />
+          </label>
 
-        <button className="w-full btn border-gray-400">
-          <span>Sign in</span>
-        </button>
-      </form>
+          <button className="w-full btn border-gray-400">
+            <span>Sign in</span>
+          </button>
 
-      <div className="mt-6 text-center text-sm">
-        Don't have an account?
-        <a
-          href="#"
-          className="text-indigo-600 hover:text-indigo-500 font-medium"
-        >
-          {" Sign in"}
-        </a>
+          <div className="mt-6 text-center text-sm">
+            Don't have an account?
+            <a href="#" className="text-accent  font-medium">
+              {" Sign in"}
+            </a>
+          </div>
+        </form>
       </div>
     </div>
   );
