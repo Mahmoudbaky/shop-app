f61b0cd95322b7d08d022a715ed7682803fcfa88
MAHMOUD <ma7mouudbaky@gmail.com>
2024-12-23 09:35:52 +0300

    finishing login desgin

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { supabase } from "@/app/utils/supabaseClient";
 import { useRouter } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
 
 const login = () => {
   const router = useRouter();
@@ -32,44 +34,43 @@ const login = () => {
   };
 
   return (
-    <div className="flex flex-row justify-center items-center h-screen ">
-      <div className="bg-neutral xl:w-[600px] md:w-[500px] h-full"></div>
-      <form
-        onSubmit={handleLogin}
-        className="col-span-3 max-w-[600px] mx-auto bg-black border-neutral border-[2px] p-8 rounded-lg shadow-lg"
-      >
-        <div className="mb-4">
-          <label className="block text-white text-sm font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full p-2 border rounded text-black"
-            required
-          />
+    <div className="flex flex-col justify-center items-center h-screen ">
+      {/* <div className="bg-neutral xl:w-[600px] md:w-[500px] h-full"></div> */}
+      <form className="space-y-4 w-[350px] ">
+        <div className=" flex flex-col gap-2">
+          <h1 className="text-2xl font-bold"> Sgin in </h1>
+          <p className="">Enter your email and password to sign in!</p>
         </div>
-        <div className="mb-6">
-          <label className="block text-white text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 border rounded text-black"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full bg-neutral text-white p-2 rounded hover:bg-blue-600 disabled:opacity-50"
-        >
-          {loading ? "Logging in..." : "Login"}
+
+        <button className="w-full btn  border-gray-400">
+          <FcGoogle className="text-2xl" />
+          <span> Google</span>
+        </button>
+        <hr className="text-red-900" />
+
+        <label className="input input-bordered flex items-center gap-2 border-gray-400">
+          Name
+          <input type="text" className="grow " placeholder="Daisy" />
+        </label>
+        <label className="input input-bordered flex items-center gap-2 border-gray-400">
+          Email
+          <input type="text" className="grow" placeholder="daisy@site.com" />
+        </label>
+
+        <button className="w-full btn border-gray-400">
+          <span>Sign in</span>
         </button>
       </form>
+
+      <div className="mt-6 text-center text-sm">
+        Don't have an account?
+        <a
+          href="#"
+          className="text-indigo-600 hover:text-indigo-500 font-medium"
+        >
+          {" Sign in"}
+        </a>
+      </div>
     </div>
   );
 };
