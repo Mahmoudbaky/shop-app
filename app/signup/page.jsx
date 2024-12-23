4b76e30e54819b515d65e2cbb07894fef8c0a1a3
MAHMOUD <ma7mouudbaky@gmail.com>
2024-12-23 12:26:22 +0300

    finshing login and signup

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { supabase } from "@/app/utils/supabaseClient";
 import { useRouter } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
 // import useAuth from "../hooks/useAuth";
 
 const SignUp = () => {
@@ -52,38 +53,101 @@ const SignUp = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col justify-center items-center gap-4 text-black"
-    >
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <input
-        type="password"
-        value={rePassword}
-        onChange={(e) => setRePassword(e.target.value)}
-        placeholder="Re enter your Password"
-      />
-      <button type="submit" className="text-white">
-        Sign Up
-      </button>
-    </form>
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center h-screen">
+      {/* /* <!-- Image Column --> */}
+      <div className="hidden bg-accent md:block md:w-full md:h-full md:my-8 xl:my-12">
+        <div className="flex flex-col items-center justify-center xl:min-w-full md:min-w-full min-h-full gap-8 md:w-[600px] xl:w-[600px]  mx-auto">
+          {/* <p className="text-2xl text-white self-start">Welcome to our shop</p> */}
+          <img
+            src="/images/rb_9090.png"
+            alt="image"
+            className="w-full h-auto xl:w-[600px] md:w-[600px]"
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-center p-8">
+        <form className="space-y-4 w-[350px] mx-auto ">
+          <div className=" flex flex-col gap-2">
+            <h1 className="text-2xl font-bold"> Sgin up </h1>
+            <p className="">Enter your information to Sign up!</p>
+          </div>
+
+          <button className="w-full btn  border-gray-400">
+            <FcGoogle className="text-2xl" />
+            <span> Google</span>
+          </button>
+          <hr className="text-red-900" />
+
+          <label
+            htmlFor="username"
+            className="input input-bordered flex items-center gap-2 border-gray-400"
+          >
+            Username
+            <input
+              type="text"
+              name="username"
+              value={username}
+              className="grow "
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label
+            htmlFor="email"
+            className="input input-bordered flex items-center gap-2 border-gray-400"
+          >
+            Email
+            <input
+              type="text"
+              name="email"
+              value={email}
+              className="grow"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label
+            htmlFor="password"
+            className="input input-bordered flex items-center gap-2 border-gray-400"
+          >
+            password
+            <input
+              type="password"
+              name="password"
+              value={password}
+              className="grow"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label
+            htmlFor="repassword"
+            className="input input-bordered flex items-center gap-2 border-gray-400"
+          >
+            password confirm
+            <input
+              type="password"
+              name="repassword"
+              value={rePassword}
+              className="grow"
+              placeholder=""
+              onChange={(e) => setRePassword(e.target.value)}
+            />
+          </label>
+
+          <button className="w-full btn border-gray-400">
+            <span>Sign up</span>
+          </button>
+
+          <div className="mt-6 text-center text-sm">
+            You already a customer?
+            <a href="#" className="text-accent  font-medium">
+              {" Sign in"}
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
